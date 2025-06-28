ff83ca45f98ddd25db22e6acfd87b850165bbd8a
zerofailure <zerofailure@gmail.com>
2025-06-28 16:34:29 -0400

    Update BuilderPage.tsx

--- components/BuilderPage.tsx
+++ components/BuilderPage.tsx
@@ -1,10 +1,7 @@
-// components/BuilderPage.tsx
 'use client';
 
-import { BuilderComponent, builder } from '@builder.io/react';
+import { BuilderComponent } from '@builder.io/react';
 
-builder.init('b6ace528dbef4807913eeec6d635af5c'); // replace with your actual key
-
-export function BuilderPage({ model = 'page' }: { model?: string }) {
-  return <BuilderComponent model={model} />;
+export function BuilderPage({ content }: { content: any }) {
+  return <BuilderComponent model="page" content={content} />;
 }
