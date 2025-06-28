c74279c6825363d35da9f0bf8627e41a60ac7ea4
zerofailure <zerofailure@gmail.com>
2025-06-27 21:10:16 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,12 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
+import { authMiddleware } from '@clerk/nextjs';
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\..*).*)",
-    "/(api|trpc)(.*)"
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
+    '/admin(.*)',
   ],
 };
