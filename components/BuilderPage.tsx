5e2773776a0dfa1b0805ad04a54369108675ebd5
zerofailure <zerofailure@gmail.com>
2025-06-28 15:47:40 -0400

    Update BuilderPage.tsx

--- components/BuilderPage.tsx
+++ components/BuilderPage.tsx
@@ -4,7 +4,7 @@
 import { BuilderComponent, builder } from '@builder.io/react';
 
 // Replace with your public API key from Builder.io
-builder.init('b6ace528dbef4807913eeec6d635af5c');
+builder.init(b6ace528dbef4807913eeec6d635af5c);
 
 export default function BuilderPage({ content }: { content: any }) {
   return (
