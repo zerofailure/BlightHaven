fe768b8a4edf46a685b40f91ad7a255429486687
zerofailure <zerofailure@gmail.com>
2025-07-02 17:07:51 -0400

    layou.tsx to add stupid builder

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
-import "./builder/builder";
 import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import "./builder/builder"; // ✅ Import custom Builder component registration
 
 export default function RootLayout({ children }) {
   return (
