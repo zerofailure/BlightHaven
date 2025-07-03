bcb94c1e555e329de46f2b4ea03e5a53654c1219
zerofailure <zerofailure@gmail.com>
2025-07-02 21:39:39 -0400

    builderregi

--- components/BuilderRegistryClient.tsx
+++ components/BuilderRegistryClient.tsx
@@ -1,5 +1,5 @@
 "use client";
-import "../../app/builder/builder"; // adjust path if needed
+import "../app/builder/builder"; // adjust path if needed
 
 export default function BuilderRegistryClient() {
   return null; // just needed for side effect of import
